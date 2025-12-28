d5e59434bde2f6e3be80a1a5f9fd00a1155c7b7d
yucolaguilar <yucolaguilar@gmail.com>
2025-12-28 14:46:22 +0000

    fix(build): correct output directory for vite build

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   root: path.resolve(__dirname, "client"),
   publicDir: path.resolve(__dirname, "client", "public"),
   build: {
-    outDir: path.resolve(__dirname, "dist"),
+    outDir: path.resolve(__dirname, "client", "dist"),
     emptyOutDir: true,
     rollupOptions: {
       output: {
